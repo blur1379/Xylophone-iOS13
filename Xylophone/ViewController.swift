91c54e5aad24e45d4499dd0c9cb43d9c169d4668
blur1379 <77322707+blur1379@users.noreply.github.com>
2022-08-08 08:10:39 +0430

    Blur completed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,25 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(sender.currentTitle ?? "C")
+    }
     
-
+    func playSound(_ note: String) {
+        let url = Bundle.main.url(forResource: note , withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
 
